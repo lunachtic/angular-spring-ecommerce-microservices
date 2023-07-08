4ee92951b00f03d60ad1abceabb1f9c5d86b7232
Loiane <loianeg@gmail.com>
2023-07-08 17:54:14 -0400

    Add properties and methods to Product class
    Updated the 'Product' class under 'product-service'. Added properties for 'id', 'name', 'description', 'category', and 'price', along with their corresponding getters and setters. These additions relates directly to the persistence layer and will be used for handling the product data in the database. Also, the 'id' property is annotated with `@Id` and `@GeneratedValue` to indicate its primary key status and auto-generation strategy.

--- ecommerce-server/product-service/src/main/java/com/loiane/product/Product.java
+++ ecommerce-server/product-service/src/main/java/com/loiane/product/Product.java
@@ -1,4 +1,60 @@
 package com.loiane.product;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+
+    private String description;
+
+    private String category;
+
+    private Double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
