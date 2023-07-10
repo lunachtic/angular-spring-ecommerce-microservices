3091052ce1e7a0a7bbc952db6fbb99f6c457ea61
Loiane <loianeg@gmail.com>
2023-07-10 18:05:28 -0400

    Add pagination and validation to ProductService, update Product entity
    Implemented pagination logic for ProductService's findAll method to improve efficiency when dealing with large datasets. As part of this, added validation constraints to page and pageSize parameters to ensure they are within acceptable ranges. Also enhanced the Product entity with new attributes and validation rules - for instance, price now must be positive, and mandatory status field was added with its default value set to 'Active'. Lastly, added SQLDelete annotation for soft deletes.

--- ecommerce-server/product-service/src/main/java/com/loiane/product/Product.java
+++ ecommerce-server/product-service/src/main/java/com/loiane/product/Product.java
@@ -1,22 +1,43 @@
 package com.loiane.product;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.hibernate.validator.constraints.Length;
 
+@SQLDelete(sql = "UPDATE Course SET status = 'Inactive' WHERE id=?")
+@Where(clause = "status <> 'Inactive'")
+@Entity
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @NotBlank
+    @Length(min = 5, max = 200)
+    @Column(length = 200, nullable = false)
     private String name;
 
-
+    @Length(max = 1000)
+    @Column(length = 1000)
     private String description;
 
+    @NotNull
+    @Column(length = 10, nullable = false)
     private String category;
 
+    @NotNull
+    @Positive
+    @Column(nullable = false)
     private Double price;
+
+    @NotNull
+    @Column(length = 8, nullable = false)
+    private String status = "Active";
 
     public Long getId() {
         return id;
@@ -56,5 +77,13 @@ public class Product {
 
     public void setPrice(Double price) {
         this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
--- ecommerce-server/product-service/src/main/java/com/loiane/product/ProductService.java
+++ ecommerce-server/product-service/src/main/java/com/loiane/product/ProductService.java
@@ -1,10 +1,17 @@
 package com.loiane.product;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
 @Service
+@Validated
 public class ProductService {
 
     private final ProductRepository productRepository;
@@ -13,7 +20,7 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    public List<Product> findAll() {
-        return productRepository.findAll();
+    public Page<Product> findAll(@PositiveOrZero int page, @Positive @Max(1000) int pageSize) {
+        return productRepository.findAll(PageRequest.of(page, pageSize));
     }
 }
