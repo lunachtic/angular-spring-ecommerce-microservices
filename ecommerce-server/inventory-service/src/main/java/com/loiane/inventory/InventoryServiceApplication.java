240053b24764946a413e9652fd1c520abc4f51c8
Loiane <loianeg@gmail.com>
2023-07-18 09:51:54 -0400

    Add inventory service module
    This commit introduces a new module 'inventory-service'. It includes creating a new 'pom.xml' for the service and configuring its dependencies. Necessary classes for testing the application, such as 'InventoryServiceApplicationTests' and 'TestInventoryServiceApplication', have been added in the 'com.loiane.inventory' package as well. Also updated 'compose.yaml' file to add the MongoDB configuration, given that the service will use MongoDB as its underlying database.

---
+++ ecommerce-server/inventory-service/src/main/java/com/loiane/inventory/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.loiane.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
---
+++ ecommerce-server/inventory-service/src/test/java/com/loiane/inventory/TestInventoryServiceApplication.java
@@ -0,0 +1,22 @@
+package com.loiane.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MongoDBContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestInventoryServiceApplication {
+
+	@Bean
+	@ServiceConnection
+	MongoDBContainer mongoDbContainer() {
+		return new MongoDBContainer("mongo:latest");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(InventoryServiceApplication::main).with(TestInventoryServiceApplication.class).run(args);
+	}
+
+}
